94e1d9e6d8ea62a30dc0c8abb170b4875ae66333
Iván Calcagno <ivancalcagno3003@gmail.com>
2023-05-21 16:21:58 -0300

    task: JS captcha

--- js/main.js
+++ js/main.js
@@ -3,16 +3,13 @@
 const min = 999;
 const max = 9999;
 
-let body = document.querySelector("#body");
-let irCaptcha = document.querySelector("#captcha");
-let captcha = document.querySelector("#container-captcha");
-let contact = document.querySelector("#contact");
 let textCaptcha = document.querySelector("#aleatorio");
 let input = document.querySelector("#numero-usuario");
 let btnCaptcha = document.querySelector("#submit");
 let icon = document.querySelector("#icon-verif");
 let btnContacto = document.querySelector("#btn-contacto");
 let warning = document.querySelector("#warning");
+let heading = document.querySelector("#heading");
 
 let ramdon;
 
@@ -34,13 +31,18 @@ function checking() {
         warning.classList.remove("ocultar");
     }else if(ramdon == input.value) {
         icon.classList.remove("bi-shield-lock-fill", "bi-shield-fill-x", "captcha-fail");
-        icon.classList.add("bi-shield-fill-check", "captcha-ok");
+        icon.classList.add("bi-shield-fill-check", "captcha-ok", "efect-ok");
         btnContacto.classList.remove("ocultar");
         warning.classList.add("ocultar");
+        input.classList.add("ocultar");
+        btnCaptcha.classList.add("ocultar");
+        heading.classList.add("ocultar");
+        textCaptcha.classList.add("ocultar");
     }else{
         icon.classList.remove("bi-shield-lock-fill", "bi-shield-fill-check", "captcha-ok");
         icon.classList.add("bi-shield-fill-x", "captcha-fail");
         btnContacto.classList.add("ocultar");
         warning.classList.remove("ocultar");
+        crear_captcha();
     }
 }
