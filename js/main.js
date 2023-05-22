ce9387a48650e9998a4746a751c2a4bfd1b6b65b
Nicolas Sanchez <nicosanchez1999@hotmail.com>
2023-05-22 17:50:20 -0300

    Nuevo css responsive y cambio en el header en el html

--- js/main.js
+++ js/main.js
@@ -1,46 +1,7 @@
-"use strict";
+"use strict"
 
-const min = 999;
-const max = 9999;
-
-let body = document.querySelector("#body");
-let irCaptcha = document.querySelector("#captcha");
-let captcha = document.querySelector("#container-captcha");
-let contact = document.querySelector("#contact");
-let textCaptcha = document.querySelector("#aleatorio");
-let input = document.querySelector("#numero-usuario");
-let btnCaptcha = document.querySelector("#submit");
-let icon = document.querySelector("#icon-verif");
-let btnContacto = document.querySelector("#btn-contacto");
-let warning = document.querySelector("#warning");
-
-let ramdon;
-
-btnCaptcha.addEventListener("click", checking);
-
-crear_captcha();
-checking();
-
-function crear_captcha() {
-    ramdon = Math.floor((Math.random()* (max - min) + min)+1);
-    textCaptcha.innerHTML = ramdon;
-}
-
-function checking() {
-    if (input.value === ''){
-        icon.classList.add("bi-shield-lock-fill");
-        icon.classList.remove("captcha-ok", "captcha-fail");
-        btnContacto.classList.add("ocultar");
-        warning.classList.remove("ocultar");
-    }else if(ramdon == input.value) {
-        icon.classList.remove("bi-shield-lock-fill", "bi-shield-fill-x", "captcha-fail");
-        icon.classList.add("bi-shield-fill-check", "captcha-ok");
-        btnContacto.classList.remove("ocultar");
-        warning.classList.add("ocultar");
-    }else{
-        icon.classList.remove("bi-shield-lock-fill", "bi-shield-fill-check", "captcha-ok");
-        icon.classList.add("bi-shield-fill-x", "captcha-fail");
-        btnContacto.classList.add("ocultar");
-        warning.classList.remove("ocultar");
-    }
-}
+let btnNav = document.querySelector("#btn-nav");
+let nav = document.querySelector("nav");
+btnNav.addEventListener("click", ()=>{
+    nav.classList.toggle("nav-mostrar");
+})
