475790e8b4b69caa85acd5e7efc89931b29b5c63
Iván Calcagno <ivancalcagno3003@gmail.com>
2023-05-24 17:25:34 -0300

    Update captcha

--- js/captcha.js
+++ js/captcha.js
@@ -3,16 +3,13 @@
 const min = 999;
 const max = 9999;
 
-let body = document.querySelector("#body");
-let irCaptcha = document.querySelector("#captcha");
-let captcha = document.querySelector("#container-captcha");
-let contact = document.querySelector("#contact");
 let textCaptcha = document.querySelector("#aleatorio");
 let input = document.querySelector("#numero-usuario");
 let btnCaptcha = document.querySelector("#submit");
 let icon = document.querySelector("#icon-verif");
 let btnContacto = document.querySelector("#btn-contacto");
 let warning = document.querySelector("#warning");
+let heading = document.querySelector("#heading");
 
 let ramdon;
 
@@ -22,25 +19,38 @@ crear_captcha();
 checking();
 
 function crear_captcha() {
-    ramdon = Math.floor((Math.random()* (max - min) + min)+1);
+    ramdon = Math.floor(Math.random() * (max - min) + min + 1);
     textCaptcha.innerHTML = ramdon;
 }
 
 function checking() {
-    if (input.value === ''){
+    if (input.value === "") {
         icon.classList.add("bi-shield-lock-fill");
         icon.classList.remove("captcha-ok", "captcha-fail");
         btnContacto.classList.add("ocultar");
         warning.classList.remove("ocultar");
-    }else if(ramdon == input.value) {
-        icon.classList.remove("bi-shield-lock-fill", "bi-shield-fill-x", "captcha-fail");
-        icon.classList.add("bi-shield-fill-check", "captcha-ok");
+    } else if (ramdon == input.value) {
+        icon.classList.remove(
+            "bi-shield-lock-fill",
+            "bi-shield-fill-x",
+            "captcha-fail"
+        );
+        icon.classList.add("bi-shield-fill-check", "captcha-ok", "efect-ok");
         btnContacto.classList.remove("ocultar");
         warning.classList.add("ocultar");
-    }else{
-        icon.classList.remove("bi-shield-lock-fill", "bi-shield-fill-check", "captcha-ok");
+        input.classList.add("ocultar");
+        btnCaptcha.classList.add("ocultar");
+        heading.classList.add("ocultar");
+        textCaptcha.classList.add("ocultar");
+    } else {
+        icon.classList.remove(
+            "bi-shield-lock-fill",
+            "bi-shield-fill-check",
+            "captcha-ok"
+        );
         icon.classList.add("bi-shield-fill-x", "captcha-fail");
         btnContacto.classList.add("ocultar");
         warning.classList.remove("ocultar");
+        crear_captcha();
     }
 }
