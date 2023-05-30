d460b91ef6916096658395fe79c14d6d75a5e574
Iván Calcagno <ivancalcagno3003@gmail.com>
2023-05-29 23:41:09 -0300

    Animaciones del header responsive

--- js/main.js
+++ js/main.js
@@ -4,4 +4,5 @@ let btnNav = document.querySelector("#btn-nav");
 let nav = document.querySelector("nav");
 btnNav.addEventListener("click", () => {
     nav.classList.toggle("nav-mostrar");
+    btnNav.classList.toggle("bi-x-circle");
 });
