ed6ef1fe141f4e57b4dbb5ef385be6247659da55
Iván Calcagno <ivancalcagno3003@gmail.com>
2023-06-02 14:57:39 -0300

    funcion scroll

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 "use strict";
 
 let btnNav = document.querySelector("#btn-nav");
-let nav = document.querySelector("nav");
+let navbar = document.querySelector("nav");
 btnNav.addEventListener("click", () => {
-    nav.classList.toggle("nav-mostrar");
+    navbar.classList.toggle("nav-mostrar");
     btnNav.classList.toggle("bi-x-circle");
 });
+
+let ubicacionPrincipal = window.pageYOffset;
+window.onscroll = function () {
+    let desplazamientoActual = window.pageYOffset;
+    if (ubicacionPrincipal >= desplazamientoActual) {
+        navbar.classList.remove("desplazamiento");
+    } else {
+        navbar.classList.add("desplazamiento");
+    }
+    ubicacionPrincipal = desplazamientoActual;
+};
