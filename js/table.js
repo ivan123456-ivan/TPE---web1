264410e258b4ffe01f6af79b229334d83a595ddc
Iván Calcagno <ivancalcagno3003@gmail.com>
2023-06-18 23:40:24 -0300

    Funcionalidad API REST inicial (agregar items y loader)

--- js/table.js
+++ js/table.js
@@ -8,39 +8,50 @@ let checkboxTable = document.querySelector("#checkbox-form");
 let inputTitulo = document.querySelector("#inputTitulo");
 let inputSubtitulo = document.querySelector("#inputSubtitulo");
 let inputDescripcion = document.querySelector("#description-table");
+let form = document.querySelector("#form");
 let advertencia = document.querySelector("#advertencia");
+const url = "https://6488ed330e2469c038fe83a4.mockapi.io/news/noticias";
 inputDescripcion.value = "";
+mostrarInformacion();
 
-let informacionTable = [
-    {
-        titulo: "Este es un titulo",
-        subtitulo: "Este es un subtitulo",
-        descripcion: "Esta es una descripcion",
-        destacado: checkboxTable.checked,
-    },
-];
-
-mostrar_tabla(0);
-
-btnEnviarTable.addEventListener("click", (e) => {
+form.addEventListener("submit", async (e) => {
     e.preventDefault();
-    if (
-        inputTitulo.value != "" &&
-        inputSubtitulo.value != "" &&
-        inputDescripcion.value != ""
-    ) {
-        containerTable.innerHTML = " ";
+    let formData = new FormData(form);
+    let titulo = formData.get("inputTitulo");
+    let subtitulo = formData.get("inputSubtitulo");
+    let descripcion = formData.get("description-table");
+    let checkbox = checkboxTable.checked;
+    if (titulo !== "" && subtitulo !== "" && descripcion !== "") {
         let nuevaInformacion = {
-            titulo: inputTitulo.value,
-            subtitulo: inputSubtitulo.value,
-            descripcion: inputDescripcion.value,
-            destacado: checkboxTable.checked,
+            titulo: titulo,
+            subtitulo: subtitulo,
+            descripcion: descripcion,
+            destacado: checkbox,
         };
-        informacionTable.push(nuevaInformacion);
-        mostrarInformacion();
-        inputTitulo.value = "";
-        inputSubtitulo.value = "";
-        inputDescripcion.value = "";
+
+        let configuracion = {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify(nuevaInformacion),
+        };
+
+        try {
+            let respuesta = await fetch(url, configuracion);
+            if (respuesta.status === 201) {
+                advertencia.classList.remove("ocultar");
+                advertencia.innerHTML = "Noticia generada correctamente!";
+                setTimeout(() => {
+                    advertencia.classList.add("ocultar");
+                }, 8000);
+                mostrarInformacion();
+            } else {
+                alert("No se pudo crear el nuevo elemento.");
+            }
+        } catch (error) {
+            console.log(error);
+        }
         advertencia.classList.add("ocultar");
     } else {
         advertencia.classList.remove("ocultar");
@@ -50,65 +61,99 @@ btnEnviarTable.addEventListener("click", (e) => {
     }
 });
 
-btnGenerarItems.addEventListener("click", (e) => {
+btnGenerarItems.addEventListener("click", async (e) => {
     e.preventDefault();
-    //let random = Math.floor(Math.random() * 3 + 1);
-    let item1 = {
+    let item = {
         titulo: "Nuevo titulo generado",
         subtitulo: "Nuevo subtitulo generado",
         descripcion: "Nueva descripcion generada",
         destacado: checkboxTable.checked,
     };
 
-    for (let i = 1; i <= 3; i++) {
-        informacionTable.push(item1);
-    }
-
-    containerTable.innerHTML = " ";
-
-    mostrarInformacion();
-});
-
-btnBorrarTable.addEventListener("click", (e) => {
-    e.preventDefault();
-    containerTable.innerHTML = " ";
-    informacionTable = [
-        {
-            titulo: "Este es un titulo",
-            subtitulo: "Este es un subtitulo",
-            descripcion: "Esta es una descripcion",
+    let configuracion = {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
         },
-    ];
-    mostrar_tabla(0);
+        body: JSON.stringify(item),
+    };
+
+    try {
+        for (let i = 1; i <= 3; i++) {
+            let respuesta = await fetch(url, configuracion);
+            if (respuesta.status == 201) {
+                advertencia.classList.remove("ocultar");
+                advertencia.innerHTML = "Items generados correctamente!";
+                setTimeout(() => {
+                    advertencia.classList.add("ocultar");
+                }, 8000);
+            } else {
+                alert("No se pudo crear el nuevo elemento.");
+            }
+        }
+        mostrarInformacion();
+    } catch (error) {
+        console.log(error);
+    }
 });
 
-function mostrar_tabla(sub) {
-    let elementos = `  
-    <tr>
-        <td>${informacionTable[sub].titulo}</td>
-        <td>${informacionTable[sub].subtitulo}</td>
-        <td>${informacionTable[sub].descripcion}</td>
-    </tr>
-    `;
-    containerTable.innerHTML += elementos;
-}
+// btnBorrarTable.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     containerTable.innerHTML = " ";
+//     informacionTable = [
+//         {
+//             titulo: "Este es un titulo",
+//             subtitulo: "Este es un subtitulo",
+//             descripcion: "Esta es una descripcion",
+//         },
+//     ];
+//     mostrar_tabla(0);
+// });
 
-function mostrar_destacado(sub) {
-    let elementosImportantes = `  
+async function mostrarInformacion() {
+    containerTable.innerHTML = `
     <tr>
-        <td class="es-importante">${informacionTable[sub].titulo}</td>
-        <td class="es-importante">${informacionTable[sub].subtitulo}</td>
-        <td class="es-importante">${informacionTable[sub].descripcion}</td>
+        <td></td>
+
+        <td>
+            <div class="container-loader">
+                <div class="loader">
+            </div>
+        </td
+
+        <td></td>
     </tr>
-    `;
-    containerTable.innerHTML += elementosImportantes;
-}
-function mostrarInformacion() {
-    for (let j = 0; j < informacionTable.length; j++) {
-        if (informacionTable[j].destacado === true) {
-            mostrar_destacado(j);
+    </div>`;
+    try {
+        let respuesta = await fetch(url);
+        if (respuesta.ok) {
+            let data = await respuesta.json();
+            containerTable.innerHTML = "";
+            for (let renglon of data) {
+                if (renglon.destacado === true) {
+                    let elementosImportantes = `  
+                    <tr>
+                        <td class="es-importante">${renglon.titulo}</td>
+                        <td class="es-importante">${renglon.subtitulo}</td>
+                        <td class="es-importante">${renglon.descripcion}</td>
+                    </tr>
+                    `;
+                    containerTable.innerHTML += elementosImportantes;
+                } else {
+                    let elementos = `  
+                    <tr>
+                        <td>${renglon.titulo}</td>
+                        <td>${renglon.subtitulo}</td>
+                        <td>${renglon.descripcion}</td>
+                    </tr>
+                    `;
+                    containerTable.innerHTML += elementos;
+                }
+            }
         } else {
-            mostrar_tabla(j);
+            console.log("error");
         }
+    } catch (error) {
+        console.log(error);
     }
 }
