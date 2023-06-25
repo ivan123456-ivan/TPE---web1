31fe174e56742118f3149bd8d86f4c2d84c379bf
Iván Calcagno <ivancalcagno3003@gmail.com>
2023-06-24 23:08:40 -0300

    tabla con createElement()

--- js/table.js
+++ js/table.js
@@ -198,20 +198,26 @@ function editarElementos() {
 }
 
 async function mostrarInformacion() {
-    containerTable.innerHTML = `
-    <tr>
-        <td></td>
-        
-        <td></td>
-        
-        <td>
-            <div class="container-loader">
-                <div class="loader">
-            </div>
-        </td
+    // containerTable.innerHTML = `
+    // <tr>
+    //     <td></td>
 
-        <td></td>
-    </tr>`;
+    //     <td></td>
+
+    //     <td>
+    //         <div class="container-loader">
+    //             <div class="loader">
+    //         </div>
+    //     </td
+
+    //     <td></td>
+    // </tr>`;
+    let containerLoader = document.createElement("div");
+    containerLoader.classList.add("container-loader");
+    containerTable.appendChild(containerLoader);
+    let loader = document.createElement("div");
+    loader.classList.add("loader");
+    containerLoader.appendChild(loader);
     try {
         let respuesta = await fetch(URL_API);
         if (respuesta.ok) {
@@ -219,31 +225,113 @@ async function mostrarInformacion() {
             containerTable.innerHTML = "";
             for (let renglon of data) {
                 if (renglon.destacado === true) {
-                    let elementosImportantes = `  
-                    <tr>
-                        <td class="es-importante">${renglon.titulo}</td>
-                        <td class="es-importante">${renglon.subtitulo}</td>
-                        <td class="es-importante">${renglon.descripcion}</td>
-                        <td class="es-importante">
-                            <button class="btn1 btn-editar" data-item="${renglon.id}"><i class="bi bi-pencil-square"></i> Editar</button>
-                            <button class="btn1 btn-eliminar" data-item="${renglon.id}"><i class="bi bi-trash"></i>Eliminar</button>
-                        </td>
-                    </tr>
-                    `;
-                    containerTable.innerHTML += elementosImportantes;
+                    // let elementosImportantes = `
+                    // <tr>
+                    //     <td class="es-importante">${renglon.titulo}</td>
+                    //     <td class="es-importante">${renglon.subtitulo}</td>
+                    //     <td class="es-importante">${renglon.descripcion}</td>
+                    //     <td class="es-importante">
+                    //         <button class="btn1 btn-editar" data-item="${renglon.id}"><i class="bi bi-pencil-square"></i> Editar</button>
+                    //         <button class="btn1 btn-eliminar" data-item="${renglon.id}"><i class="bi bi-trash"></i>Eliminar</button>
+                    //     </td>
+                    // </tr>
+                    // `;
+                    // creacion de fila de noticia
+                    let tr = document.createElement("tr");
+                    let titulo = document.createElement("td");
+                    titulo.textContent = renglon.titulo;
+                    titulo.classList.add("es-importante");
+                    let subtitulo = document.createElement("td");
+                    subtitulo.textContent = renglon.subtitulo;
+                    subtitulo.classList.add("es-importante");
+                    let descripcion = document.createElement("td");
+                    descripcion.textContent = renglon.descripcion;
+                    descripcion.classList.add("es-importante");
+
+                    // botonera
+                    let botonera = document.createElement("td");
+                    botonera.classList.add("es-importante");
+
+                    //boton editar
+                    let btn1 = document.createElement("button");
+                    btn1.classList.add("btn1", "btn-editar");
+                    btn1.setAttribute("data-item", `${renglon.id}`);
+                    btn1.textContent = "Editar ";
+                    let i1 = document.createElement("i");
+                    i1.classList.add("bi", "bi-pencil-square");
+                    btn1.appendChild(i1);
+
+                    //boton eliminar
+                    let btn2 = document.createElement("button");
+                    btn2.classList.add("btn1", "btn-eliminar");
+                    btn2.textContent = "Eliminar ";
+                    let i2 = document.createElement("i");
+                    i2.classList.add("bi", "bi-trash");
+                    btn2.appendChild(i2);
+                    btn2.setAttribute("data-item", `${renglon.id}`);
+
+                    botonera.appendChild(btn1);
+                    botonera.appendChild(btn2);
+
+                    //creacion de la fila y sus elementos
+
+                    tr.appendChild(titulo);
+                    tr.appendChild(subtitulo);
+                    tr.appendChild(descripcion);
+                    tr.appendChild(botonera);
+                    containerTable.appendChild(tr);
                 } else {
-                    let elementos = `  
-                    <tr>
-                        <td>${renglon.titulo}</td>
-                        <td>${renglon.subtitulo}</td>
-                        <td>${renglon.descripcion}</td>
-                        <td>
-                            <button class="btn1 btn-editar" data-item="${renglon.id}"><i class="bi bi-pencil-square"></i> Editar</button>
-                            <button class="btn1 btn-eliminar" data-item="${renglon.id}"><i class="bi bi-trash"></i> Eliminar</button>
-                        </td>
-                    </tr>
-                    `;
-                    containerTable.innerHTML += elementos;
+                    // let elementos = `
+                    // <tr>
+                    //     <td>${renglon.titulo}</td>
+                    //     <td>${renglon.subtitulo}</td>
+                    //     <td>${renglon.descripcion}</td>
+                    //     <td>
+                    //         <button class="btn1 btn-editar" data-item="${renglon.id}"><i class="bi bi-pencil-square"></i> Editar</button>
+                    //         <button class="btn1 btn-eliminar" data-item="${renglon.id}"><i class="bi bi-trash"></i> Eliminar</button>
+                    //     </td>
+                    // </tr>
+                    // `;
+                    // creacion de fila de noticia
+                    let tr = document.createElement("tr");
+                    let titulo = document.createElement("td");
+                    titulo.textContent = renglon.titulo;
+                    let subtitulo = document.createElement("td");
+                    subtitulo.textContent = renglon.subtitulo;
+                    let descripcion = document.createElement("td");
+                    descripcion.textContent = renglon.descripcion;
+
+                    // botonera
+                    let botonera = document.createElement("td");
+
+                    //boton editar
+                    let btn1 = document.createElement("button");
+                    btn1.classList.add("btn1", "btn-editar");
+                    btn1.setAttribute("data-item", `${renglon.id}`);
+                    btn1.textContent = "Editar ";
+                    let i1 = document.createElement("i");
+                    i1.classList.add("bi", "bi-pencil-square");
+                    btn1.appendChild(i1);
+
+                    //boton eliminar
+                    let btn2 = document.createElement("button");
+                    btn2.classList.add("btn1", "btn-eliminar");
+                    btn2.textContent = "Eliminar ";
+                    let i2 = document.createElement("i");
+                    i2.classList.add("bi", "bi-trash");
+                    btn2.appendChild(i2);
+                    btn2.setAttribute("data-item", `${renglon.id}`);
+
+                    botonera.appendChild(btn1);
+                    botonera.appendChild(btn2);
+
+                    //creacion de la fila y sus elementos
+
+                    tr.appendChild(titulo);
+                    tr.appendChild(subtitulo);
+                    tr.appendChild(descripcion);
+                    tr.appendChild(botonera);
+                    containerTable.appendChild(tr);
                 }
             }
             eliminarElementos();
