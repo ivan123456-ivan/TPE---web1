517b23a91eda8de67915e6e8dda9df3b24a7d95c
Iván Calcagno <ivancalcagno3003@gmail.com>
2023-06-28 21:28:26 -0300

    Acomodando sistema de comentarios a todos los html

--- js/coments.js
+++ js/coments.js
@@ -1,8 +1,8 @@
 "use strict";
 
-
 let form = document.querySelector("#form-coments");
 let container = document.querySelector(".coments-container");
+let advertencia = document.querySelector("#advertencia");
 let url = "https://649c87880480757192384e48.mockapi.io/commentsInformation";
 mostrar();
 form.addEventListener("submit", async (e) => {
@@ -11,38 +11,45 @@ form.addEventListener("submit", async (e) => {
     let nombre = formData.get("nombre");
     let email = formData.get("email");
     let comentarios = formData.get("comentarios");
-    if (
-        nombre != "" &&
-        email != "" &&
-        comentarios != ""
-    ) {
+    if (nombre != "" && email != "" && comentarios != "") {
         let fecha = new Date();
         let hora = fecha.getHours();
         let minutos = fecha.getMinutes();
 
         let nuevoComentario = {
-            "nombre": nombre,
-            "email": email,
-            "comentario": comentarios,
-            "time": [hora, minutos],
+            nombre: nombre,
+            email: email,
+            comentario: comentarios,
+            time: [hora, minutos],
         };
         let config = {
             method: "POST",
             headers: {
                 "Content-type": "application/json",
             },
-            body: JSON.stringify(nuevoComentario)
+            body: JSON.stringify(nuevoComentario),
         };
         try {
             let response = await fetch(url, config);
-            if(response.status === 201){
+            if (response.status === 201) {
+                advertencia.classList.remove("ocultar");
+                advertencia.innerHTML = "Comentario registrado correctamente!";
+                setTimeout(() => {
+                    advertencia.classList.add("ocultar");
+                }, 8000);
                 mostrar();
             }
         } catch (error) {
             console.log(error);
         }
+    } else {
+        advertencia.classList.remove("ocultar");
+        advertencia.innerHTML = "Debe completar todos los campos!";
+        setTimeout(() => {
+            advertencia.classList.add("ocultar");
+        }, 8000);
     }
-})
+});
 
 async function mostrar() {
     let response = await fetch(url);
@@ -53,14 +60,11 @@ async function mostrar() {
     }
 }
 
-
-
 async function crearComentario(data, pos) {
-
     let article = document.createElement("article");
     let divCommentHeader = document.createElement("div");
     let pHeader = document.createElement("p");
-    let span = document.createElement("span"); 
+    let span = document.createElement("span");
     let divCommentInfo = document.createElement("div");
     let pCommentInfo = document.createElement("p");
     let divCommentFotter = document.createElement("div");
@@ -69,7 +73,6 @@ async function crearComentario(data, pos) {
     article.classList.add("coment");
     divCommentHeader.classList.add("coment-header");
 
-    
     divCommentInfo.classList.add("coment-info");
     divCommentFotter.classList.add("coment-footer");
     span.classList.add("resaltado");
@@ -81,11 +84,11 @@ async function crearComentario(data, pos) {
     span.textContent = `${data[pos].nombre}`;
     pHeader.insertAdjacentElement("beforebegin", span);
     pHeader.textContent += ` - ${data[pos].email}`;
-    
+
     article.appendChild(divCommentInfo);
     divCommentInfo.appendChild(pCommentInfo);
-    pCommentInfo.textContent =  `${data[pos].comentario} `;
+    pCommentInfo.textContent = `${data[pos].comentario} `;
     article.appendChild(divCommentFotter);
     divCommentFotter.appendChild(timeCommentFotter);
-    timeCommentFotter.textContent =  `Comentó a las ${data[pos].time[0]}:${data[pos].time[1]}`;
+    timeCommentFotter.textContent = `Comentó a las ${data[pos].time[0]}:${data[pos].time[1]}`;
 }
